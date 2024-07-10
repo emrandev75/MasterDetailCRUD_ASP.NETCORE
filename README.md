cf8adb6ff5a415ada4de6c25ad24e59cba6ae46c
emrandev75 <emranhossen.dev@gmail.com>
2024-07-10 12:08:03 +0600

    allCode added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MasterDetailCRUD_ASP.NETCORE
+# Master-Detail-CRUD-ASP.NETCore
+![core](https://github.com/EmranHossen75/Master-Detail-CRUD-ASP.NETCore/assets/142440452/621be1d3-391f-47ab-9604-df32fc75ab30)
---
+++ work_01/Controllers/SkillsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using work_01.Models;
+
+namespace work_01.Controllers
+{
+    public class SkillsController : Controller
+    {
+        private readonly CandidateDbContext _context;
+
+        public SkillsController(CandidateDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+              return _context.Skills != null ? 
+                          View(await _context.Skills.ToListAsync()) :
+                          Problem("Entity set 'CandidateDbContext.Skills'  is null.");
+        }
+       
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SkillsId,SkillsName")] Skills skills)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(skills);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(skills);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Skills == null)
+            {
+                return NotFound();
+            }
+
+            var skills = await _context.Skills.FindAsync(id);
+            if (skills == null)
+            {
+                return NotFound();
+            }
+            return View(skills);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SkillsId,SkillsName")] Skills skills)
+        {
+            if (id != skills.SkillsId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(skills);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SkillsExists(skills.SkillsId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(skills);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Skills == null)
+            {
+                return NotFound();
+            }
+
+            var skills = await _context.Skills
+                .FirstOrDefaultAsync(m => m.SkillsId == id);
+            if (skills == null)
+            {
+                return NotFound();
+            }
+
+            return View(skills);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Skills == null)
+            {
+                return Problem("Entity set 'CandidateDbContext.Skills'  is null.");
+            }
+            var skills = await _context.Skills.FindAsync(id);
+            if (skills != null)
+            {
+                _context.Skills.Remove(skills);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SkillsExists(int id)
+        {
+          return (_context.Skills?.Any(e => e.SkillsId == id)).GetValueOrDefault();
+        }
+    }
+}
